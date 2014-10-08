4c63e6efdb183382b19129e022fc2c4545896e95
Mjiulee <limj123@189.cn>
2014-10-08 17:23:27 +0800

    还是登陆不上，调试求解

--- cloud/app.js
+++ cloud/app.js
@@ -1,11 +1,16 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
-
+var avosExpressCookieSession = require('avos-express-cookie-session');
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
+
+//启用cookie
+app.use(express.cookieParser('Your Cookie Secure'));
+//使用avos-express-cookie-session记录登录信息到cookie。
+app.use(avosExpressCookieSession({ cookie: { maxAge: 3600000 }}));
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
@@ -17,13 +22,15 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-	AV.User.logIn(req.body.username,req.body.password).then(function(){
-			res.redirect('/profile');
-		},
-		function(error){
-			res.redirect('login');
-		}
-	);
+	AV.User.logIn(req.body.username, req.body.password).then(function() {
+      //登录成功，avosExpressCookieSession会自动将登录用户信息存储到cookie
+      console.log('signin successfully: %j', AV.User.current());
+      //跳转到profile页面。
+      res.redirect('/profile');
+    },function(error) {
+      //登录失败，跳转到登录页面
+      res.redirect('/login');
+  });
 });
 
 app.get('/profile', function(req, res) {
